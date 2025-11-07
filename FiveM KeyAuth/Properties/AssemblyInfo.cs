f2c074b2330cc1b8bfacb66aca4813bf5cde06a9
Denis T. <deadspell21391@gmail.com>
2025-11-07 12:05:25 +0100

    AssemblyInfo.cs

---
+++ FiveM KeyAuth/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("FiveM KeyAuth")]
+[assembly: AssemblyDescription("KeyAuth Loader")]
+[assembly: AssemblyConfiguration("FiveM")]
+[assembly: AssemblyCompany("KeyAuth LLC")]
+[assembly: AssemblyProduct("Loader")]
+[assembly: AssemblyCopyright("Copyright © KeyAuth")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("B6641B04-064E-48EA-89CC-8E4DDDEA7385")]
+[assembly: AssemblyVersion("3.48.662.2408")]
+[assembly: AssemblyFileVersion("3.48.662.2408")]
+[assembly: NeutralResourcesLanguage("en-US")]
